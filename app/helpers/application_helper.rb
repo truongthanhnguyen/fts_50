d6d849788f9c4ee8d3d83ba7bab4b7f3afe9113f
thanh <truongthanh.bk56@gmail.com>
2016-02-05 09:56:59 +0700

    refs #52852 user create/edit/destroy question

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,15 @@ module ApplicationHelper
     base_title = t "home_page.full_title"
     page_title.empty? ? base_title: "#{page_title} | #{base_title}"
   end
+
+  def add_fields_answer f, association
+    new_object = f.object.class.reflect_on_association(association).klass.new
+    tmpl = f.fields_for(association, new_object,
+      child_index: "new_#{association}") do |b|
+      render "#{association.to_s.singularize}_form", f: b
+    end
+    tmpl = tmpl.gsub /(?<!\n)\n(?!\n)/, " "
+    return "<script> var #{association.to_s}_form = '#{tmpl.to_s}' </script>"
+      .html_safe
+  end
 end
